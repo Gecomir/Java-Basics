f704c8638468200dca7fcfb3dd29c2c7fa724cc6
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-03-31 18:15:55 +0300

    Add files via upload

---
+++ Nested-Loops/CinemaTickets.java
@@ -0,0 +1,11 @@
+package NestedLoops.lab;
+
+import java.util.Scanner;
+
+public class CinemaTickets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
