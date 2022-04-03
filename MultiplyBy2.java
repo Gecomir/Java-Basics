0fd799cb506296618c121e8633082a610e72a20d
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-04-03 18:02:24 +0300

    Add files via upload

---
+++ MultiplyBy2.java
@@ -0,0 +1,21 @@
+package PoSlojniProverki;
+
+import java.util.Scanner;
+
+public class MultiplyBy2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num = Double.parseDouble(scanner.nextLine());
+        double result = 0.0;
+
+        while (num >= 0) {
+            result = num * 2;
+            System.out.printf("Result: %.2f%n", result);
+            num = Double.parseDouble(scanner.nextLine());
+        }
+        if (num < 0) {
+            System.out.println("Negative number!");
+        }
+    }
+}
