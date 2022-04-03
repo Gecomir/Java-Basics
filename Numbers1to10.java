bb3399840be707a88a54d2df7de54a33d5d419ad
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-04-03 18:02:46 +0300

    Add files via upload

---
+++ Numbers1to10.java
@@ -0,0 +1,10 @@
+package PoSlojniProverki;
+
+public class Numbers1to10 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=10; i++) {
+            System.out.println(i);
+        }
+    }
+}
